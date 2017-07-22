7dde48c160e9de98dde1ccf06c83ebf0a8fd0676
Infernio <infernio@me.com>
2017-07-22 02:13:23 +0200

    Positioned the MCM logo correctly

--- sync_files.sh
+++ sync_files.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+cp -rf interface "D:\Steam\steamapps\common\Skyrim\Data"
 cp -rf scripts "D:\Steam\steamapps\common\Skyrim\Data"
 cp -rf textures "D:\Steam\steamapps\common\Skyrim\Data"
 

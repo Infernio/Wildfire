27f1b92c264fbc80b9e049e50a6a80530b38aee9
Infernio <infernio@icloud.com>
2021-05-06 10:39:14 +0200

    Drop ancient scripts

--- sync_files.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-cp -rf "Interface" "D:\Steam\steamapps\common\Skyrim\Data"
-cp -rf "scripts" "D:\Steam\steamapps\common\Skyrim\Data"
-cp -rf "textures" "D:\Steam\steamapps\common\Skyrim\Data"
-
-cp -f "D:\Steam\steamapps\common\Skyrim\Data\Wildfire.esp" "Wildfire.esp"
